51bd91b78eda59c9943e980b9c3972f5e7922fb2
ashukla1707-beep <a.shukla.1707@gmail.com>
2026-08-28 13:54:23 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='stat-archive-v121';
+const ASSETS=['./','./index.html','./manifest.json'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;
+e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
