5c8e65917d0bcb67fb2417dd7b0c83e011ac4b95
ashukla1707-beep <a.shukla.1707@gmail.com>
2026-09-05 01:42:16 +0530

    Refresh cache for archive divider spacing match

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "stat-archive-shell-v20260905-archive-breathing-v7";
+const CACHE = "stat-archive-shell-v20260905-archive-divider-gap-v8";
 
 const APP_SHELL = [
   "./",
@@ -37,7 +37,7 @@ const MENU_FLASH_GUARD = `
 const FEATURE_SCRIPT_TAG = '<script src="./assets/js/feature-polish.js?v=20260905-5"></script>';
 const HERO_FIX_SCRIPT_TAG = '<script src="./assets/js/hero-layout-fix.js?v=20260901-4"></script>';
 const HERO_SELECTION_GUARD_TAG = '<script src="./assets/js/hero-selection-guard.js?v=20260901-3"></script>';
-const ACTION_SPACING_FIX_TAG = '<script src="./assets/js/action-spacing-fix.js?v=20260905-2"></script>';
+const ACTION_SPACING_FIX_TAG = '<script src="./assets/js/action-spacing-fix.js?v=20260905-3"></script>';
 
 function decorateNavigationHtml(html) {
   let out = html;
