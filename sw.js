8c8e0bacbc398ca696813ac4160dc8505b47d06c
ashukla1707-beep <a.shukla.1707@gmail.com>
2026-09-05 13:05:05 +0530

    Refresh cache for Archive Entries divider alignment

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "stat-archive-shell-v20260905-signed-in-spacing-revert-v15";
+const CACHE = "stat-archive-shell-v20260905-archive-divider-align-v16";
 const EXTERNAL_CACHE = "stat-archive-external-v1";
 
 const APP_SHELL = [
@@ -39,7 +39,7 @@ const MENU_FLASH_GUARD = `
 const FEATURE_SCRIPT_TAG = '<script src="./assets/js/feature-polish.js?v=20260905-5"></script>';
 const HERO_FIX_SCRIPT_TAG = '<script src="./assets/js/hero-layout-fix.js?v=20260901-4"></script>';
 const HERO_SELECTION_GUARD_TAG = '<script src="./assets/js/hero-selection-guard.js?v=20260901-3"></script>';
-const ACTION_SPACING_FIX_TAG = '<script src="./assets/js/action-spacing-fix.js?v=20260905-8"></script>';
+const ACTION_SPACING_FIX_TAG = '<script src="./assets/js/action-spacing-fix.js?v=20260905-9"></script>';
 const SPEED_SCRIPT_TAG = '<script src="./assets/js/speed-boost.js?v=20260905-1"></script>';
 
 function decorateNavigationHtml(html) {
