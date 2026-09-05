822cf0764850d2b513e7221df55fafd79eb0251a
ashukla1707-beep <a.shukla.1707@gmail.com>
2026-09-06 00:48:46 +0530

    Force refined PDF pinch viewer cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "stat-archive-shell-v20260906-pdf-stable-v37";
+const CACHE = "stat-archive-shell-v20260906-pdf-refined-v38";
 const EXTERNAL_CACHE = "stat-archive-external-v2";
 
 const APP_SHELL = [
@@ -50,7 +50,7 @@ const DOWNLOAD_FIX_TAG = '<script src="./assets/js/download-fix.js?v=20260905-1"
 const SEARCH_SUGGESTIONS_TAG = '<script src="./assets/js/search-suggestions.js?v=20260905-4"></script>';
 const SEARCH_FILTER_FIX_TAG = '<script src="./assets/js/search-filter-fix.js?v=20260905-1"></script>';
 const PDF_TITLE_FIX_TAG = '<script src="./assets/js/pdf-title-fix.js?v=20260905-1"></script>';
-const PDF_PREVIEW_V2_TAG = '<script src="./assets/js/pdf-preview-v2.js?v=20260906-3"></script>';
+const PDF_PREVIEW_V2_TAG = '<script src="./assets/js/pdf-preview-v2.js?v=20260906-4"></script>';
 
 function decorateNavigationHtml(html) {
   let out = html;
