73102bff2a59adcce3c7a661a4f1cd05c7879adc
ashukla1707-beep <a.shukla.1707@gmail.com>
2026-09-09 20:24:42 +0530

    Bump offline library entry formatter version

--- assets/js/offline-library-heading-search-fix.js
+++ assets/js/offline-library-heading-search-fix.js
@@ -282,7 +282,7 @@ body[data-theme="light"] #offlineLibraryOverlay #offlineSubjectSelect{
   function loadEntryFormatter() {
     if (document.querySelector('script[data-sa-offline-entry-format="1"]')) return;
     const script = document.createElement('script');
-    script.src = './assets/js/offline-library-entry-format.js?v=20260909-1';
+    script.src = './assets/js/offline-library-entry-format.js?v=20260909-2';
     script.async = false;
     script.dataset.saOfflineEntryFormat = '1';
     document.body.appendChild(script);
